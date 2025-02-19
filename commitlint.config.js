46e3dea61027ef9c5bf794f37365cdd5bcb3ccf7
Stephen <hellostephenwm@gmail.com>
2025-02-19 13:45:24 -0500

    style: apply linting and formatting using ESLint and Prettier

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,14 +6,14 @@ export default {
 	/**
 	 * Resolve and load @commitlint/config-conventional from node_modules.
 	 */
-	extends: ["@commitlint/config-conventional"],
+	extends: ['@commitlint/config-conventional'],
 	/**
 	 * Resolve and load @commitlint/format from node_modules.
 	 */
-	formatter: "@commitlint/format",
+	formatter: '@commitlint/format',
 	/**
 	 * Custom URL to show upon failure.
 	 */
 	helpUrl:
-		"https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
+		'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
 };
