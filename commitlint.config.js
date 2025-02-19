b71dcea44ed0cced4d4b15c87623cfc9da81903d
Stephen <hellostephenwm@gmail.com>
2025-02-18 22:12:01 -0500

    chore(commitlint): add commitlint configuration for enforcing commit message conventions

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+/**
+ * Commitlint main configuration file.
+ * Referenced packages must be installed.
+ */
+export default {
+	/**
+	 * Resolve and load @commitlint/config-conventional from node_modules.
+	 */
+	extends: ["@commitlint/config-conventional"],
+	/**
+	 * Resolve and load @commitlint/format from node_modules.
+	 */
+	formatter: "@commitlint/format",
+	/**
+	 * Custom URL to show upon failure.
+	 */
+	helpUrl:
+		"https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
+};
