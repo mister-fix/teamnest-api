33203ffa2c13511c2bdb34531f4e5bc4e2597c5a
Stephen <hellostephenwm@gmail.com>
2025-02-20 00:12:02 -0500

    feat: add initial Plop.js generator setup

---
+++ plopfile.js
@@ -0,0 +1,9 @@
+/**
+ * Plop main configuration file.
+ * @param {object} plop - The Plop API used to configure generators.
+ * @returns {void} - This function does not return any value.
+ */
+export default function (/** @type {import('plop').NodePlopAPI} */ plop) {
+	// Welcome message
+	plop.setWelcomeMessage('Select a generator');
+}
