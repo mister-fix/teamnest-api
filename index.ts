f14947bf244057ec05e0a9208c45c604be118d21
Stephen <hellostephenwm@gmail.com>
2025-02-22 15:32:55 -0500

    feat: add server.on listening event, utilizing custom logger utility

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 
 import app from '@/app';
+import logger from '@/utils/logger';
 
 // Load environment variables
 config();
@@ -12,29 +13,26 @@ const host = process.env.HOST ?? '0.0.0.0'; // '0.0.0.0' is production fallback
 const port: number = Number(process.env.PORT) || 3000;
 
 const startServer = (portToUse: number) => {
-	const server = app.listen(portToUse, host, () => {
-		// eslint-disable-next-line no-console
-		console.log(`Server running on: http://${host}:${portToUse}`);
+	const server = app.listen(portToUse, host);
+
+	server.on('listening', () => {
+		logger.info(`Server running on: http://${host}:${portToUse}`);
 	});
 
 	server.on('error', (error: NodeJS.ErrnoException) => {
 		// If port is already in use, try the next port
 		if (error?.code === 'EADDRINUSE') {
-			// eslint-disable-next-line no-console
-			console.warn(`Port ${portToUse} is in use. Trying another port...`);
+			logger.warn(`Port ${portToUse} is in use. Trying another port...`);
 			startServer(portToUse + 1); // Try the next port
 		} else {
 			// Handle other errors
-			// eslint-disable-next-line no-console
-			console.log('Server encountered an error: ', error);
+			logger.error('Server encountered an error: ', error);
 		}
 	});
 
 	server.on('close', () => {
-		// eslint-disable-next-line no-console
-		console.log('Shutting down server...');
-		// eslint-disable-next-line no-console
-		console.log('Cleaning up resources...');
+		logger.info('Shutting down server...');
+		logger.info('Cleaning up resources...');
 	});
 };
 
