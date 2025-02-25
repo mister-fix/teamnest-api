27d96d1d71a1ee53b2ddb1bd818a70f1973eb985
Stephen <hellostephenwm@gmail.com>
2025-02-25 16:13:30 -0500

    chore: deleted unused plopfile.js (incorrect file extension)

--- plopfile.js
+++
@@ -1,9 +0,0 @@
-/**
- * Plop main configuration file.
- * @param {object} plop - The Plop API used to configure generators.
- * @returns {void} - This function does not return any value.
- */
-export default function (/** @type {import('plop').NodePlopAPI} */ plop) {
-	// Welcome message
-	plop.setWelcomeMessage('Select a generator');
-}
